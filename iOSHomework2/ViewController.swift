49c0f4b585b769c3cbd36911793387ae9f2e04e9
abdullah <Ab.alsayegh2004@gmail.com>
2020-06-23 16:08:03 +0300

    donw hw2

--- iOSHomework2/ViewController.swift
+++ iOSHomework2/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         let member = nameTextField.text!
         
         // MARK: -   1️⃣ تحت الخط membersNamesArray إلي المصفوفه memberقم بإضافة المتغير
+        membersNamesArray.append(member)
         
         
         
@@ -34,7 +35,7 @@ class ViewController: UIViewController {
         
         // MARK: -  4️⃣ functionCall داخل المتغير  secretNameLetter قم باستدعاء الدالة
         
-        var functionCall = ""
+        var functionCall = secretNameLetter(array: membersNamesArray)
         
         
         // MARK: -   النهاية
@@ -52,7 +53,7 @@ class ViewController: UIViewController {
         
         // MARK: -  5️⃣ functionCall داخل المتغير  secretNameEmoji قم باستدعاء الدالة
         
-        var functionCall = ""
+        var functionCall = secretNameEmoji(array: membersNamesArray)
         
         // MARK: -   النهاية
         
@@ -65,7 +66,18 @@ class ViewController: UIViewController {
     
     
     // MARK: - 2️⃣ تحت هذا الخط secretNameLetter قم بكتابة الداله
-    
+    func secretNameLetter(array: [String]) -> String{
+        
+        
+        var result = ""
+                
+        for i in array
+        {
+             result = result + i.prefix(1)
+            
+        }
+        return result
+    }
     
     // MARK: -   النهاية
     
@@ -74,11 +86,36 @@ class ViewController: UIViewController {
     
     
     // MARK: - 3️⃣ تحت هذا الخط secretNameEmoji قم بكتابة الداله
-    
+    func secretNameEmoji(array: [String]) -> String {
+        
+        var letterEmoji = ["a":"👨🏻‍🦳" , "b": "🦊" , "c": "✏️" , "d": "🤡" ,"e": "🤕" , "f":"🤖", "i": "😺", "h": "🤠" , "k": "😽" , "j": "💀", "m": "👾", "l": "👁", "o": "🧠", "n": "🦷", "q": "👩🏼‍🍳", "p": "👓", "s": "🐸", "r": "🐵", "u": "🦁", "t": "🐥", "w": "🐳", "v": "🐡", "y": "🐙", "x": "🐎", "z": "🦍"]
+        
+        var emojiResults = ""
+        
+        var names = ""
+        
+        for m in array {
+            
+            names = String(m.prefix(1))
+            
+            for x in letterEmoji {
+                
+                if names == x.key {
+                    
+                    emojiResults += x.value
+                    
+                }
+                
+            }
+            
+        }
+        return emojiResults
+       
+        }
+        
+
     
     // MARK: -   النهاية
-    
-    
     
 }
 
